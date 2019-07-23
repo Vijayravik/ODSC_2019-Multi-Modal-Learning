0b317ae65d794d74e8e931b04702f007a061ef20
adib0073 <adib0073@gmail.com>
2019-07-23 15:05:43 +0530

    ODSC content updated with Confusion matrix

---
+++ confusion_matrix_audio_data.py
@@ -0,0 +1,33 @@
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    #plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    s = [['TN','FP'], ['FN', 'TP']]
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(2):
+        for j in range(2):
+             plt.text(j,i, (str(s[i][j])+" = "+str(format(cm[i][j],fmt))),horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+def get_confusion_matrix_for_audio_data():
+    cm = np.load('audio_weights.npy')
+    print(cm)
+    print(cm.shape)
+    plt.figure()
+    plot_confusion_matrix(cm, classes=['Negative','Positive'],   title='Confusion matrix for audio data')
+    plt.show()
